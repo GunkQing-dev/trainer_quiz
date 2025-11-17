4a8a1beb66271601ea31c6c93eb949252a90a0fc
Quilliano van Hal <quilliano.van.hal@gmail.com>
2025-11-17 15:09:03 +0100

    Update top_contact_links.dart

--- lib/widgets/top_contact_links.dart
+++ lib/widgets/top_contact_links.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:trainer_quiz/data/external_resources.dart';
-import 'package:trainer_quiz/widgets/external_resource_links.dart';
+import 'package:trainer_quiz/utils/url_helpers.dart';
 
 class TopContactLinks extends StatelessWidget {
   const TopContactLinks({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ExternalResourceLinks(
-      resources: maintainerResources,
-      compact: true,
+    final theme = Theme.of(context);
+    final borderColor = theme.colorScheme.outline.withOpacity(0.4);
+
+    return Wrap(
       alignment: WrapAlignment.end,
-      compactMaxWidth: 180,
+      spacing: 12,
+      runSpacing: 8,
+      children: maintainerResources.map((resource) {
+        final icon = SvgPicture.asset(
+          resource.assetPath,
+          width: 20,
+          height: 20,
+          colorFilter: resource.tintIcon
+              ? const ColorFilter.mode(Colors.white, BlendMode.srcIn)
+              : null,
+        );
+
+        return Tooltip(
+          message: resource.subtitle ?? resource.label,
+          child: Material(
+            color: resource.color,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(999),
+              side: BorderSide(color: borderColor),
+            ),
+            child: InkWell(
+              onTap: () => launchExternalUrl(resource.url),
+              borderRadius: BorderRadius.circular(999),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: icon,
+              ),
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
